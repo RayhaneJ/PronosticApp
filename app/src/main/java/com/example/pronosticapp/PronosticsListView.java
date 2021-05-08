62b7bddc2f697e0b4c21cd29e16e2adec5fd295c
rayhaneJ <rayhane.jebbari@3ds.com>
2021-05-08 22:50:04 +0200

    rencontres list view

---
+++ app/src/main/java/com/example/pronosticapp/PronosticsListView.java
@@ -0,0 +1,14 @@
+package com.example.pronosticapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PronosticsListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pronostics_list_view);
+    }
+}
