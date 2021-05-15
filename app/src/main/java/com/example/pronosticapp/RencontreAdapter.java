f706e11088d2ced68802368ec77b9c1ba7c76da9
rayhaneJ <jebbarirayhane@gmail.com>
2021-05-15 23:12:20 +0200

    delete modify in pronostics

--- app/src/main/java/com/example/pronosticapp/RencontreAdapter.java
+++ app/src/main/java/com/example/pronosticapp/RencontreAdapter.java
@@ -1,11 +1,17 @@
 package com.example.pronosticapp;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -49,6 +55,44 @@ public class RencontreAdapter extends BaseAdapter {
         equipeVisTextView.setText(rencontres.get(position).getEquipeVisiteur());
         TextView equipeFav = (TextView)convertView.findViewById(R.id.equipeFav);
         equipeFav.setText(rencontres.get(position).getEquipeFavorite() + " Vainqueur");
+
+        final Animation animation = AnimationUtils.loadAnimation(parent.getContext(),
+                R.anim.slide_out);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                rencontres.remove(position);
+                notifyDataSetChanged();
+            }
+        });
+
+        Button buttonSupprimer = (Button) convertView.findViewById(R.id.supprimer);
+        View finalConvertView = convertView;
+        buttonSupprimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finalConvertView.startAnimation(animation);
+            }
+        });
+
+        Button modifierButton = (Button) convertView.findViewById(R.id.modifier);
+        modifierButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(parent.getContext(), ModifierRencontre.class);
+                intent.putExtra("rencontreId", rencontres.get(position).getId());
+                ((Activity)parent.getContext()).startActivityForResult(intent, 1);
+            }
+        });
+
         return convertView;
     }
 }
