b3c3445ab665b7df002e0efe6389027ec1ba9f37
rayhaneJ <jebbarirayhane@gmail.com>
2021-05-15 23:12:20 +0200

    delete modify in pronostics

--- app/src/main/java/com/example/pronosticapp/Pronostics.java
+++ app/src/main/java/com/example/pronosticapp/Pronostics.java
@@ -1,6 +1,9 @@
 package com.example.pronosticapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -16,47 +19,41 @@ public class Pronostics extends AppCompatActivity {
     private PronosticDbContext pronosticDbContext;
     private ArrayList<Rencontre> rencontres;
     private RencontreAdapter rencontreAdapter;
-    private int selectedRow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pronostics);
         setTitle("Rencontres");
+        pronosticDbContext = new PronosticDbContext(getApplicationContext());
         rencontres = new ArrayList<>();
-        rencontres.add(new Rencontre("Rencontre des titans", "10/20/09", "Ligue des champions", "PSG", "Lyon", "PSG"));
-        rencontres.add(new Rencontre("Rencontre des null", "10/20/09", "Ligue des null", "Lyon", "PSG", "Lyon"));
-        rencontres.add(new Rencontre("Rencontre des mandarins", "10/20/09", "Ligue des mandarins", "Lille", "Lyon", "Lyon"));
+        long id1 = pronosticDbContext.insertRencontre(new Rencontre("Rencontre des titans", "10/20/09", "Ligue des champions", "PSG", "Lyon", "PSG"));
+        long id2 = pronosticDbContext.insertRencontre(new Rencontre("Rencontre des null", "10/20/09", "Ligue des null", "Lyon", "PSG", "Lyon"));
+        long id3 = pronosticDbContext.insertRencontre(new Rencontre("Rencontre des mandarins", "10/20/09", "Ligue des mandarins", "Lille", "Lyon", "Lyon"));
+        rencontres.add(new Rencontre(id1, "Rencontre des titans", "10/20/09", "Ligue des champions", "PSG", "Lyon", "PSG"));
+        rencontres.add(new Rencontre(id2, "Rencontre des null", "10/20/09", "Ligue des null", "Lyon", "PSG", "Lyon"));
+        rencontres.add(new Rencontre(id3, "Rencontre des mandarins", "10/20/09", "Ligue des mandarins", "Lille", "Lyon", "Lyon"));
         rencontresListView = (ListView)findViewById(R.id.rencontresListView);
         rencontreAdapter = new RencontreAdapter(getApplicationContext(), rencontres);
         rencontresListView.setAdapter(rencontreAdapter);
-
-        final Animation animation = AnimationUtils.loadAnimation(this,
-                R.anim.slide_out);
-        animation.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                rencontres.remove(selectedRow);
-                rencontreAdapter.notifyDataSetChanged();
-            }
-        });
-
-        rencontresListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                view.startAnimation(animation);
-                selectedRow = i;
-            }
-        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                Rencontre rencontre = pronosticDbContext.getRencontre((int)data.getLongExtra("rencontreIdToUpdate", 0));
+                for(int i = 0; i< rencontres.size(); i++){
+                    if(rencontres.get(i).getId() == rencontre.getId()){
+                        rencontres.set(i, rencontre);
+                    }
+                }
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+
+            }
+        }
+    }
 }
